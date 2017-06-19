5ecfd954a957090ade6eeaf1990e3c2b7679f3d7
zhuYuShun <zhuYuShun666666@163.com>
2017-06-19 10:08:26 +0800

    built

---
+++ Aha-Algorithm/Aha-Algorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Aha-Algorithm
+//
+//  Created by Mac on 2017/6/19.
+//  Copyright © 2017年 oldSix. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
